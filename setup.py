54cfe0c6048f0d162b1ab4786c63494c2bc5b206
maksimt <maksimtsikhanovich@gmail.com>
2018-02-01 18:49:27 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rri_nmf',
+    description='Non-negative matrix factorization using rank-one residue '
+                'iterations.',
+
+    version='0.1',
+    author='Maksim Tsikhanovich',
+    url='https://github.com/maksimt/rri_nmf',
+
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'}
+)
---
+++ src/rri_nmf/__init__.py
@@ -0,0 +1,1 @@
+from rri_nmf.nmf import *
